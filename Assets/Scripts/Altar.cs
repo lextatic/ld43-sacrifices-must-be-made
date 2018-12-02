2b6fb6b6132c13dc301e58c2340a465a994eb96a
Alexandre Melotti <lex.melotti@gmail.com>
2018-12-01 22:00:59 -0200

    Complete endgame flow

--- Assets/Scripts/Altar.cs
+++ Assets/Scripts/Altar.cs
@@ -1,15 +1,12 @@
 ﻿using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static Inventory;
 
 public class Altar : MonoBehaviour
 {
-	private int victoryCondition;
-	private int sacrificesCount;
-
-	public Inventory playerInventory;
-	public SpriteRenderer playerSacrificeSprite;
-
 	[Serializable]
 	public struct TypeAndPrefab
 	{
@@ -17,13 +14,48 @@ public class Altar : MonoBehaviour
 		public GameObject Prefab;
 	}
 
+	[Serializable]
+	public struct TextAndBackground
+	{
+		public string Dialog;
+		public GameObject DialogBackground;
+	}
+
+	private int victoryCondition;
+	private int sacrificesCount;
+	private int dialogCount;
+	private bool endGame;
+	private bool enableRestart;
+
+	public Inventory playerInventory;
+	public SpriteRenderer playerSacrificeSprite;
+	public InputComponent PlayerInput;
+
 	public TypeAndPrefab[] TypeAndPrefabs;
+
+	public GameObject MeowDialogue;
+	public GameObject ClosedDoor;
+
+	public GameObject OriginalTilemap;
+	public GameObject AnimationTilemaps;
+
+	public TextMeshPro TextMesh;
+	public GameObject DialogBackground;
+	public GameObject ScreamDialogBackground;
+	public TextAndBackground[] Dialogs;
+
+	public SpriteRenderer EndGameFadeSprite;
+
+	public GameObject EndTexts;
 
 	public void Start()
 	{
 		var enemies = FindObjectsOfType<EnemyType>();
 		victoryCondition = enemies.Length;
 		sacrificesCount = 0;
+		dialogCount = 0;
+		endGame = false;
+		enableRestart = false;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -48,9 +80,108 @@ public class Altar : MonoBehaviour
 
 				if(sacrificesCount == victoryCondition)
 				{
-					Debug.Log("Victory!");
+					PlayerInput.Jump = false;
+					PlayerInput.Run = false;
+					PlayerInput.Horizontal = 0;
+
+					PlayerInput.enabled = false;
+
+					StartCoroutine(PlayEndGame());
 				}
 			}
+		}
+	}
+
+	private IEnumerator PlayEndGame()
+	{
+		yield return new WaitForSeconds(1f);
+
+		MeowDialogue.SetActive(true);
+
+		yield return new WaitForSeconds(2f);
+
+		MeowDialogue.SetActive(false);
+
+		yield return new WaitForSeconds(1f);
+
+		ShowNextDialog();
+
+		yield return new WaitForSeconds(2f);
+
+		HideDialog();
+
+		yield return new WaitForSeconds(3f);
+
+		OriginalTilemap.SetActive(false);
+		AnimationTilemaps.SetActive(true);
+
+		ClosedDoor.SetActive(false);
+
+		yield return new WaitForSeconds(1f);
+
+		ShowNextDialog();
+
+		yield return new WaitForSeconds(2f);
+
+		HideDialog();
+
+		yield return new WaitForSeconds(.5f);
+
+		ShowNextDialog();
+
+		yield return new WaitForSeconds(1f);
+
+		PlayerInput.Horizontal = -1;
+
+		yield return new WaitForSeconds(1f);
+
+		HideDialog();
+
+		yield return new WaitForSeconds(1f);
+
+		ClosedDoor.SetActive(true);
+
+		yield return new WaitForSeconds(1f);
+
+		ShowNextDialog();
+
+		yield return new WaitForSeconds(2f);
+
+		endGame = true;
+
+		yield return new WaitForSeconds(4f);
+
+		HideDialog();
+		EndTexts.SetActive(true);
+
+		enableRestart = true;
+	}
+
+	private void ShowNextDialog()
+	{
+		TextMesh.text = Dialogs[dialogCount].Dialog;
+		Dialogs[dialogCount].DialogBackground.SetActive(true);
+		TextMesh.gameObject.SetActive(true);
+		dialogCount++;
+	}
+
+	private void HideDialog()
+	{
+		DialogBackground.SetActive(false);
+		ScreamDialogBackground.SetActive(false);
+		TextMesh.gameObject.SetActive(false);
+	}
+
+	public void Update()
+	{
+		if(endGame)
+		{
+			EndGameFadeSprite.color = Color.Lerp(EndGameFadeSprite.color, Color.black, Time.deltaTime * 2f);
+		}
+
+		if(enableRestart && Input.anyKeyDown)
+		{
+			SceneManager.LoadScene(0);
 		}
 	}
 }
---
+++ Assets/Scripts/BlinkingText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class BlinkingText : MonoBehaviour
+{
+	private TextMeshPro TextMesh;
+	private Color targetColor;
+	private Color baseColor;
+	private bool fading;
+	private float startTime;
+
+	public float blinkDuration = 2f;
+	public Color fullColor;
+	public Color fadedColor;
+
+	void Start()
+    {
+		TextMesh = GetComponent<TextMeshPro>();
+		fading = false;
+		baseColor = fullColor;
+		targetColor = fadedColor;
+		TextMesh.color = fullColor;
+
+		StartCoroutine(BlinkLoop());
+	}
+
+    void Update()
+    {
+		TextMesh.color = Color.Lerp(baseColor, targetColor, ((Time.time - startTime) / blinkDuration));
+    }
+
+	public IEnumerator BlinkLoop()
+	{
+		startTime = Time.time;
+
+		yield return new WaitForSeconds(blinkDuration);
+
+		if(fading)
+		{
+			targetColor = fullColor;
+			baseColor = fadedColor;
+		}
+		else
+		{
+			targetColor = fadedColor;
+			baseColor = fullColor;
+		}
+
+		fading = !fading;
+
+		StartCoroutine(BlinkLoop());
+	}
+}
